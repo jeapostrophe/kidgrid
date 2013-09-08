4908c1a18c6f96ad2ec3620ee5b598b291ad6d5e
Jay McCarthy <jay.mccarthy@gmail.com>
2013-09-08 09:23:52 -0600

    up

--- main.rkt
+++ main.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
 (require racket/runtime-path
+         racket/list
          web-server/servlet
          web-server/servlet-env
          (prefix-in d: racket/date))
@@ -30,6 +31,12 @@
 
 (define-runtime-path source-dir ".")
 
+(define (week-date)
+  (d:date->string (d:current-date)))
+
+(module+ test
+  (displayln (week-date)))
+
 (define (go users activities)
   (define live-activities
     (filter (λ (a)
@@ -48,38 +55,60 @@
                [type "text/css"]
                [href "/style.css"])))
        (body
-        (table
-         (thead
-          (tr
-           (th "Activity")
-           ,@(for/list ([u (in-list users)])
-               `(th ,(user-name u)))))
-         (tbody
-          ,@(for/list ([a (in-list live-activities)])
-              `(tr (td ,(activity-name a))
-                   ,@(for/list ([u (in-list users)])
-                       (if (activity-live? a u)
-                         `(td
-                           (table
-                            (tr ,@(for/list ([i (in-range (activity-freq a))])
-                                    `(td nbsp)))))
-                         `(td ([class "blank"]) nbsp)))))))))))
-
+        (div
+         ([id "container"])
+         (div
+          ([id "header"])
+          (h3 "Week of " ,(week-date)))
+         (div
+          ([id "body"])
+          (table
+           ([id "activities"])
+           (thead
+            (tr
+             ,@(for/list ([u (in-list users)])
+                 `(th ,(user-name u)))))
+           (tbody
+            ,@(append*
+               (for/list ([a (in-list live-activities)])
+                 (list
+                  `(tr ([class "activity"])
+                       (td ([colspan ,(number->string (length users))])
+                           ,(activity-name a)))
+                  `(tr
+                    ,@(for/list ([u (in-list users)])
+                        (if (activity-live? a u)
+                          `(td
+                            (table ([id "days"])
+                                   (tr ,@(for/list ([i (in-range (activity-freq a))])
+                                           `(td
+                                             (table ([id "day"])
+                                                    (tr
+                                                     ,@(for/list ([j (in-range 3)])
+                                                         `(td nbsp)))))))))
+                          `(td ([class "blank"]) nbsp))))))))))
+         (div ([id "footer"])
+              ,@(for/list ([i (in-range 21)])
+                  `(span ,(number->string i) nbsp))))))))
+  
   (serve/servlet
    start
+   #:listen-ip #f
+   #:command-line? #t
+   #:servlet-regexp #rx"^/$"
    #:extra-files-paths (list (build-path source-dir "static"))
-   #:port 0))
+   #:port 9007))
 
 (module+ main
   (define ??? 18.0)
   (go
    (list
-    (user "Peach" 
-          3.0 #;
-          (date 2010 08 23))
     (user "Frog"
           5.0 #;
-          (date 2008 09 09)))
+          (date 2008 09 09))
+    (user "Peach"
+          3.0 #;
+          (date 2010 08 23)))
    (list
     (activity 3.0 3.5 5 "Reading > Letters")
     (activity 3.0 3.5 5 "Math > Numbers")
@@ -87,6 +116,7 @@
     (activity 3.5 4.0 5 "Reading > Letters [written]")
     (activity 3.5 4.0 5 "Math > Numbers [written]")
     (activity 3.5 ??? 5 "Reading > Blends")
+    (activity 4.0 ??? 5 "Piano")
     (activity 4.0 ??? 5 "Reading > Words [written]")
     (activity 4.0 ??? 5 "Math > Addition [10x10] [written]")
     (activity 4.0 ??? 3 "Computer > Keyboard & Mouse")
---
+++ sync.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync -h --progress -a --delete -L --exclude db --exclude compiled --exclude .git . plt-etc:local/kidgrid/
