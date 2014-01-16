9c6c4807243449c54e6ea271ad84644d0434cbe5
Jay McCarthy <jay.mccarthy@gmail.com>
2014-01-16 13:47:27 -0700

    staticize

--- main.rkt
+++ main.rkt
@@ -1,8 +1,7 @@
 #lang racket/base
 (require racket/runtime-path
          racket/list
-         web-server/servlet
-         web-server/servlet-env
+         xml
          (prefix-in d: racket/date))
 
 (struct date (year month day))
@@ -31,13 +30,7 @@
 
 (define-runtime-path source-dir ".")
 
-(define (week-date)
-  (d:date->string (d:current-date)))
-
-(module+ test
-  (displayln (week-date)))
-
-(define (go users activities)
+(define (go output-p users activities)
   (define live-activities
     (filter (λ (a)
               (ormap (λ (u) (activity-live? a u))
@@ -46,76 +39,77 @@
   (define max-freq
     (list-max (map activity-freq live-activities)))
 
-  (define (start req)
-    (response/xexpr
-     `(html
-       (head
-        (title "Kid Grid")
-        (link ([rel "stylesheet"]
-               [type "text/css"]
-               [href "/style.css"])))
-       (body
+  (define xe
+    `(html
+      (head
+       (title "Kid Grid")
+       (link ([rel "stylesheet"]
+              [type "text/css"]
+              [href "style.css"])))
+      (body
+       (div
+        ([id "container"])
         (div
-         ([id "container"])
-         (div
-          ([id "header"])
-          (h3 "Week of " ,(week-date)))
-         (div
-          ([id "body"])
-          (table
-           ([id "activities"])
-           (tbody
-            ,@(append*
-               (for/list ([a (in-list live-activities)])
-                 (list
-                  `(tr ([class "activity"])
-                       (td ([colspan ,(number->string (length users))])
-                           ,(activity-name a)))
-                  `(tr
-                    ,@(for/list ([u (in-list users)])
-                        (if (activity-live? a u)
-                          `(td
-                            (table ([id "days"])
-                                   (tr ,@(for/list ([i (in-range (activity-freq a))])
-                                           `(td
-                                             (table ([id "day"])
-                                                    (tr
-                                                     ,@(for/list ([j (in-range 3)])
-                                                         `(td nbsp)))))))))
-                          `(td ([class "blank"]) nbsp))))))))))
-         (div ([id "footer"])
-              ,@(for/list ([i (in-range 21)])
-                  `(span ,(number->string i) nbsp))))))))
-  
-  (serve/servlet
-   start
-   #:listen-ip #f
-   #:command-line? #t
-   #:servlet-regexp #rx"^/$"
-   #:extra-files-paths (list (build-path source-dir "static"))
-   #:port 9007))
+         ([id "header"])
+         (h3 "Week of "
+             (script
+              "document.write(new Date().toDateString());")))
+        (div
+         ([id "body"])
+         (table
+          ([id "activities"])
+          (tbody
+           ,@(append*
+              (for/list ([a (in-list live-activities)])
+                (list
+                 `(tr ([class "activity"])
+                      (td ([colspan ,(number->string (length users))])
+                          ,(activity-name a)))
+                 `(tr
+                   ,@(for/list ([u (in-list users)])
+                       (if (activity-live? a u)
+                         `(td
+                           (table ([id "days"])
+                                  (tr ,@(for/list ([i (in-range (activity-freq a))])
+                                          `(td
+                                            (table ([id "day"])
+                                                   (tr
+                                                    ,@(for/list ([j (in-range 3)])
+                                                        `(td nbsp)))))))))
+                         `(td ([class "blank"]) nbsp))))))))))
+        (div ([id "footer"])
+             ,@(for/list ([i (in-range 21)])
+                 `(span ,(number->string i) nbsp)))))))
+
+  (with-output-to-file output-p
+    #:exists 'replace
+    (λ () (write-xexpr xe))))
 
 (module+ main
+  (require racket/runtime-path)
+  (define-runtime-path index.html "static/index.html")
+
   (define ??? 18.0)
   (go
+   index.html
    (list
     (user "Frog"
           5.0)
     (user "Peach"
-          3.0))
+          3.25))
    (list
     (activity 3.0 3.5 5 "Reading > Letters")
     (activity 3.0 3.5 5 "Math > Numbers")
     ;; (activity 3.0 ??? 1 "Life Skills")
     ;; (activity 3.0 3.5 3 "Reading > Learning Books")
     (activity 3.0 3.5 3 "Writing > Tracing")
+    (activity 3.25 ??? 3 "Computer > Keyboard & Mouse")
     (activity 3.5 4.0 5 "Reading > Letters [written]")
     (activity 3.5 4.0 5 "Math > Numbers [written]")
     (activity 3.5 ??? 5 "Reading > Blends")
     (activity 4.0 ??? 3 "Music")
     (activity 4.0 ??? 5 "Reading > Words [written]")
     (activity 4.0 ??? 5 "Math > Addition [10x10] [written]")
-    (activity 4.0 ??? 3 "Computer > Keyboard & Mouse")
     (activity 4.5 ??? 5 "Reading > Beginning reader books (to parent)")
     (activity 5.0 ??? 5 "Math > Subtraction [10x10] [written]")
     (activity 5.0 ??? 3 "Science > Basic reading (w/ parent)")
--- sync.sh
+++ sync.sh
@@ -1,3 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
+. ~/.zshrc
 
-rsync -h --progress -a --delete -L --exclude db --exclude compiled --exclude .git . plt-etc:local/kidgrid/
+DEST=~blogs/jeapostrophe.github.com-ng/blog/family
+
+cd ~github/kidgrid
+
+racket -t main.rkt
+cp -fr static/* $DEST/kidgrid/
+
+cd $DEST
+git add .
+git commit -m "kidgrid" . || true
+git push
+git gc
