c2130f8a51ef27ae1e391c6027d6b9a9d8dc8396
Jay McCarthy <jay.mccarthy@gmail.com>
2014-03-10 06:26:44 -0600

    up

--- main.rkt
+++ main.rkt
@@ -7,7 +7,7 @@
 
 (struct date (year month day))
 (struct user (name bday))
-(struct activity (start-age end-age freq name))
+(struct activity (id except never-died? start-age end-age freq name))
 
 (define (date->seconds d)
   (d:find-seconds 0 0 0 (date-day d) (date-month d) (date-year d)))
@@ -73,13 +73,42 @@
           (tbody
            (tr ([class "boxes"])
                ,@(for/list ([u (in-list users)])
-                   (define died-as
-                     (filter (curry activity-died? u) activities))
+                   (define all-live-for-u
+                     (map activity-id
+                          (filter (λ (a) (activity-live? a u))
+                                  live-activities)))
+                   (define (live-sibling? a)
+                     (member (activity-id a)
+                             all-live-for-u))
+                   (define-values (died-as live-as)
+                     (partition
+                      (curry activity-died? u)
+                      activities))
+                   (define-values (live-sibling still-died-as)
+                     (partition
+                      live-sibling?
+                      died-as))
+                   (define-values (except-u still-still-died-as)
+                     (partition
+                      (λ (a)
+                        (member u (activity-except a)))
+                      still-died-as))
+                   (define-values (nd still-still-still-died-as)
+                     (partition
+                      activity-never-died?
+                      still-still-died-as))
+                   (define final-died-as
+                     still-still-still-died-as)
+                   (printf "~a: Ignored ~v\n"
+                           (user-name u)
+                           (map activity-name
+                                (append live-sibling except-u nd)))
                    `(td ([class "died"])
-                     (table
-                      (tr
-                       (td ,(user-name u))
-                       (td ,@(map (λ (a) `(span ,(activity-name a))) died-as)))))))
+                        (table
+                         (tr
+                          (td ,(user-name u))
+                          (td ,@(map (λ (a) `(span ,(activity-name a))) 
+                                     final-died-as)))))))
            ,@(append*
               (for/list ([a (in-list live-activities)])
                 (list
@@ -87,25 +116,25 @@
                       (td ([colspan ,(number->string (length users))])
                           ,(activity-name a)))
                  `(tr ([class "boxes"])
-                   ,@(for/list ([u (in-list users)])
-                       (if (activity-live? a u)
-                         `(td
-                           (table ([id "days"])
-                                  (tr ,@(add-spacing
-                                         (for/list ([i (in-range (activity-freq a))])
-                                           `(td
-                                             (table ([id "day"])
-                                                    (tr
-                                                     ,@(for/list ([j (in-range 3)])
-                                                         `(td nbsp))))))))))
-                         `(td ([class "blank"]) nbsp))))))))))
+                      ,@(for/list ([u (in-list users)])
+                          (if (activity-live? a u)
+                            `(td
+                              (table ([id "days"])
+                                     (tr ,@(add-spacing
+                                            (for/list ([i (in-range (activity-freq a))])
+                                              `(td
+                                                (table ([id "day"])
+                                                       (tr
+                                                        ,@(for/list ([j (in-range 3)])
+                                                            `(td nbsp))))))))))
+                            `(td ([class "blank"]) nbsp))))))))))
         (div ([id "footer"])
              ,@(for/list ([i (in-range 21)])
                  `(span ,(number->string i) nbsp)))))))
 
   (with-output-to-file output-p
     #:exists 'replace
-    (λ () 
+    (λ ()
       (displayln "<!DOCTYPE html>")
       (write-xexpr xe))))
 
@@ -113,30 +142,67 @@
   (require racket/runtime-path)
   (define-runtime-path index.html "static/index.html")
 
+  (define current-id
+    (make-parameter (λ () (gensym))))
+  (define current-except-users
+    (make-parameter empty))
+  (define current-never-died
+    (make-parameter #f))
+  (define-syntax-rule (*activity . a)
+    (activity ((current-id)) (current-except-users) (current-never-died)
+              . a))
+
+  (define-syntax-rule (meta-activity a ...)
+    (let ([the-id (gensym)])
+      (parameterize ([current-id (λ () the-id)])
+        (list a ...))))
+  (define-syntax-rule (except-user u a ...)
+    (parameterize ([current-except-users (list u)])
+      (list a ...)))
+  (define-syntax-rule (never-died a ...)
+    (parameterize ([current-never-died #t])
+      (list a ...)))
+
+
+  (define u:frog
+    (user "Frog"
+          5.5))
+  (define u:peach
+    (user "Peach"
+          3.5))
+
   (define ??? 18.0)
   (go
    index.html
    (list
-    (user "Frog"
-          5.0)
-    (user "Peach"
-          3.5))
-   (list
-    (activity 3.0 3.5 5 "Reading > Letters")
-    (activity 3.0 3.5 5 "Math > Numbers")
-    ;; (activity 3.0 ??? 1 "Life Skills")
-    ;; (activity 3.0 3.5 3 "Reading > Learning Books")
-    (activity 3.0 3.5 3 "Writing > Tracing")
-    (activity 3.25 ??? 3 "Computer > Keyboard & Mouse")
-    (activity 3.5 4.0 5 "Reading > Letters [written]")
-    (activity 3.5 4.0 5 "Math > Numbers [written]")
-    (activity 3.5 ??? 5 "Reading > Blends")
-    (activity 4.0 ??? 3 "Music")
-    (activity 4.0 ??? 5 "Reading > Words [written]")
-    (activity 4.0 5.0 5 "Math > Addition [10x10] [written]")
-    (activity 4.5 ??? 5 "Reading > Beginning reader books (to parent)")
-    (activity 5.0 ??? 5 "Math > Subtraction [10x10] [written]")
-    (activity 5.0 ??? 3 "Science > Basic reading (w/ parent)")
-    (activity 5.0 ??? 3 "History > Basic reading (w/ parent)")
-    (activity 5.0 ??? 5 "Literature > Basic reading (w/ parent)")
-    (activity 5.0 ??? 5 "Composition > Sentences [written]"))))
+    u:frog
+    u:peach)
+   (flatten
+    (list
+     (*activity 3.0 3.5 5 "Reading > Letters")
+     (*activity 3.0 3.5 5 "Math > Numbers")
+     ;; (*activity 3.0 ??? 1 "Life Skills")
+     ;; (*activity 3.0 3.5 3 "Reading > Learning Books")
+     (never-died
+      (*activity 3.0 3.5 3 "Writing > Tracing"))
+     (*activity 3.25 ??? 3 "Computer > Keyboard & Mouse")
+     (except-user u:frog
+                  (*activity 3.5 4.0 5 "Reading > Letters [written]")
+                  (*activity 3.5 4.0 5 "Math > Numbers [written]"))
+     (*activity 3.5 5.5 5 "Reading > Blends")
+     (*activity 4.0 ??? 5 "Reading > Words [written]")
+     (*activity 4.0 5.0 5 "Math > Addition [10x10] [written]")
+     (meta-activity
+      (*activity 4.5 5.5 5 "Reading > Beginning reader books (to parent) [5s]")
+      (*activity 5.5 ??? 5 "Reading > Beginning reader books (to parent) [10s]"))
+     (*activity 5.0 ??? 5 "Math > Subtraction [10x10] [written]")
+     (meta-activity
+      (*activity 5.0 5.5 3 "Science > Basic reading (w/ parent)")
+      (*activity 5.5 ??? 5 "Science > Basic reading (w/ parent)"))
+     (meta-activity
+      (*activity 5.0 5.5 3 "History > Basic reading (w/ parent)")
+      (*activity 5.5 ??? 5 "History > Basic reading (w/ parent)"))
+     (*activity 5.0 ??? 5 "Literature > Basic reading (w/ parent)")
+     (*activity 5.0 ??? 5 "Composition > Sentences [written]")
+     (*activity 5.5 ??? 5 "Math > Addition [10x10] [memory]")
+     (*activity 6.0 ??? 3 "Music")))))
